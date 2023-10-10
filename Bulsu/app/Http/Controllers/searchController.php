078046447ed644d88f3d94950c454f1ef379813a
ranvoxrat <markfeliciano36@gmail.com>
2023-10-11 07:22:07 +0800

    Latest Update

--- Bulsu/app/Http/Controllers/searchController.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\Controller;
-use function Laravel\Prompts\search;
-use App\Hotspot;
-use App\Scene;
-class searchController extends Controller{
-  
-
-}
--- Bulsu/resources/views/admin/createAccount.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.main')
-
-@section('title', 'Create Account')
-
-@section('content')
- <h1>hello</h1>
-@endsection
-  
