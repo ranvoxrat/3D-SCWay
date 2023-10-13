e273269eebf042a5ccef26c5366e631232efdd6c
ranvoxrat <markfeliciano36@gmail.com>
2023-10-14 00:06:54 +0800

    final project

---
+++ storage/framework/views/b4291daf13c17233f42396b551782149defd7f23.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo e(asset('css/login.css')); ?>">
+    <link rel="stylesheet" href="<?php echo e(asset('fontawesome-free-6.4.0-web/css/all.min.css')); ?>">
+</head>
+<body>
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <?php echo $__env->make('admin.layouts.scripts', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</body>
+</html>
+
+<?php /**PATH C:\xampp\htdocs\3D-SC\resources\views/auth/layouts/main.blade.php ENDPATH**/ ?>
